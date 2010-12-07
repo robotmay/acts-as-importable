683504ac150732870c0e56b9177338bb7db9f331
Robert May <rob@core-webdesign.co.uk>
2010-12-07 14:50:13 +0000

    Fixing errors in Rails 3

--- lib/core_extensions.rb
+++ lib/core_extensions.rb
@@ -1,7 +1,7 @@
 # See http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/212639
 # And http://blade.nagaokaut.ac.jp/cgi-bin/scat.rb/ruby/ruby-talk/210633 for alternative [:id] notation
 class ActiveRecord::Base
-  alias_method :id__, :id
+  alias_method :id__, :id if method_defined? :id
 end
 
 class Object
